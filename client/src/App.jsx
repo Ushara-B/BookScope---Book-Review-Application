db132754042fdcc675a5ba09ad47cad70c19cb97
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-12-23 23:36:25 +0530

    initialization commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> welcome Bookscope</h1>
+        
+    </>
+  )
+}
+
+export default App
